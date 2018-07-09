5b1e100275c5e72c03f05f570282130707cdb78d
Alex Chernov <alexusblack@gmail.com>
2018-07-09 20:26:23 +1000

    min unit nav

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CubeController : MonoBehaviour {
+	public Camera camera;
+	public NavMeshAgent agent;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitController : MonoBehaviour {
+	public Camera camera;
+	public GameObject selection;
+	public bool canMove = true;
+
+	private NavMeshAgent agent;
+	private bool selected;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(selected && canMove && Input.GetMouseButton(0)) {
+			var ray = camera.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)) {
+				agent.SetDestination(hit.point);
+			}
+		}
+	}
+
+
+	void OnMouseDown() {
+		if(selection != null) {
+			selection.SetActive(true);
+			selected = true;
+		}
+	}
+}
