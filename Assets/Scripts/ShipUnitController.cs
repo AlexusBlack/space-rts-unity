fecff0076523b96efbd30143d1a94c3404a64af8
Alex Chernov <alexusblack@gmail.com>
2018-07-13 14:18:08 +1000

    better controllers logic separation

---
+++ Assets/Scripts/ShipUnitController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ShipUnitController : MonoBehaviour {
+	public Camera Camera;
+	
+	public bool canMove = true;
+
+	private NavMeshAgent agent;
+	private UnitController unit;
+	
+
+	// Use this for initialization
+	void Start () {
+		unit = GetComponent<UnitController>();
+		if(unit == null) {
+			Debug.LogError("Ship Unit Controller requires Unit Controller");
+		}
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(unit.Selected && canMove && Input.GetMouseButton(1)) {
+			var ray = Camera.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)) {
+				agent.SetDestination(hit.point);
+			}
+		}
+	}
+}
--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -1,44 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+
 
 public class UnitController : MonoBehaviour {
-	public PlayerController owner;
-	public Camera cam;
-	public GameObject selection;
-	public bool canMove = true;
+	public PlayerController Owner;
+	public GameObject Selection;
+	public bool Selected { get { return selected; } }
 
-	private NavMeshAgent agent;
 	private bool selected;
 
 	public void Select() {
 		selected = true;
-		if(selection != null) {
-			selection.SetActive(true);
+		if(Selection != null) {
+			Selection.SetActive(true);
 		}
 	}
 
 	public void Deselect() {
 		selected = false;
-		if(selection != null) {
-			selection.SetActive(false);
-		}
-	}
-
-	// Use this for initialization
-	void Start () {
-		agent = GetComponent<NavMeshAgent>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(selected && canMove && Input.GetMouseButton(1)) {
-			var ray = cam.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-			if(Physics.Raycast(ray, out hit)) {
-				agent.SetDestination(hit.point);
-			}
+		if(Selection != null) {
+			Selection.SetActive(false);
 		}
 	}
 
@@ -46,7 +28,7 @@ public class UnitController : MonoBehaviour {
 		if(Input.GetMouseButton(0)) {
 			Select();
 		}
-		owner.SetSelected(gameObject);
-	}
+		Owner.SetSelected(gameObject);
+	}	
 
 }
