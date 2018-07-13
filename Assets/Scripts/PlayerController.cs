b05db71fdcec6c25789feb7f80e7633047f98c24
Alex Chernov <alexusblack@gmail.com>
2018-07-13 15:55:08 +1000

    unit selection box ui

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,25 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-	public GameObject UnitPanel;
-	public GameObject[] units;
-	public GameObject[] selectedUnits;
+public class PlayerController : MonoBehaviour
+{
+  public GameObject UnitPanel;
+  public GameObject[] units;
+  public GameObject[] selectedUnits;
 
-	public void SetSelected(GameObject unit) {
-		foreach(var oldSelectedUnit in selectedUnits) {
-			oldSelectedUnit.GetComponent<UnitController>().Deselect();
-		}
-		selectedUnits = new [] { unit };
-		unit.GetComponent<UnitController>().Select();
-		UnitPanel.SetActive(true);
-	}
+  private bool isSelecting = false;
+  private Vector3 selectStartMousePosition;
 
-	public void DeselectAll() {
-		foreach(var oldSelectedUnit in selectedUnits) {
-			oldSelectedUnit.GetComponent<UnitController>().Deselect();
+  public void SetSelected(GameObject unit)
+  {
+    foreach (var oldSelectedUnit in selectedUnits)
+    {
+      oldSelectedUnit.GetComponent<UnitController>().Deselect();
+    }
+    selectedUnits = new[] { unit };
+    unit.GetComponent<UnitController>().Select();
+    UnitPanel.SetActive(true);
+  }
+
+  public void DeselectAll()
+  {
+    foreach (var oldSelectedUnit in selectedUnits)
+    {
+      oldSelectedUnit.GetComponent<UnitController>().Deselect();
+    }
+    selectedUnits = new GameObject[] { };
+    UnitPanel.SetActive(false);
+  }
+
+  void Update()
+  {
+    // If we press the left mouse button, save mouse location and begin selection
+    if (Input.GetMouseButtonDown(0))
+    {
+      isSelecting = true;
+      selectStartMousePosition = Input.mousePosition;
+    }
+    // If we let go of the left mouse button, end selection
+    if (Input.GetMouseButtonUp(0)) {
+      isSelecting = false;
 		}
-		selectedUnits = new GameObject[] { };
-		UnitPanel.SetActive(true);
-	}
+  }
+
+  void OnGUI()
+  {
+    if( isSelecting )
+		{
+			// Create a rect from both mouse positions
+			var rect = Utils.GetScreenRect(selectStartMousePosition, Input.mousePosition );
+			Utils.DrawScreenRect( rect, new Color( 0.8f, 0.8f, 0.95f, 0.25f ) );
+			Utils.DrawScreenRectBorder( rect, 2, new Color( 0.8f, 0.8f, 0.95f ) );
+		}
+  }
+
+	
 }
